35339fac58fc4aecf99c1f44db1b3714ca431bfc
Benjamin Mutie <benjaminmutie53@gmail.com>
2023-09-19 13:24:13 +0300

    the code actually works it was my dumb self

--- Authorization.py
+++ Authorization.py
@@ -3,49 +3,60 @@ import time
 import random
 from requests.exceptions import ConnectionError
 
+
 # generating an access token
 def generate_token():
-    url = 'http://DESKTOP-LJL56QO:9443/token'
+    url = 'http://localhost:9000/token'
 
     headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Encoding': 'gzip, deflate'
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Accept': 'application/json',
+
     }
 
     data = {'grant_type': 'password',
-            # 'username': 'Admin',
+            # 'username': 'admin',
             # 'password': 'a',
-            'scope': 'apiKey=bc4acf10-16eb-4f8b-8bba-160d5a30908b'
-
+            'scope': 'apikey=04284baa-8323-4775-913f-beaffa7664d8'
             }
-    # this will take an random time beween 3 and 8 seconds before retrying
-    time_inactive_before_retry = random.randint(3,8)
+
+    # response = requests.post(url=url, headers=headers,data=data)
+    # response_status = response.raise_for_status()
+    # print('response status:', response.json())
+    #
+    # print('response status:', response_status)
+
+    # this will take an random time between 3 and 8 seconds before retrying
+    time_inactive_before_retry = random.randint(3, 5)
     # this is the number of retires to be made
-    MAX_RETRIES = 5
+    MAX_RETRIES = 3
     for attempt in range(MAX_RETRIES):
         try:
-            response = requests.post(url=url, headers=headers, data=data)
-            response.raise_for_status()
-            break
+            response = requests.post(url=url, headers=headers ,data=data)
+            print('response status:', response.json())
+            return response.json()
+
         except ConnectionError as ce:
             print(f"Connection error (attempt {attempt + 1}/{MAX_RETRIES}): {ce}")
-            if attempt <  MAX_RETRIES -1:
-                time.sleep(time_inactive_before_retry)
+            if attempt < MAX_RETRIES - 1:
                 print("Retrying...")
+                time.sleep(time_inactive_before_retry)
+
             else:
-                print("Max retries reached,unable to connect:\n",ce)
+                print("Max retries reached,unable to connect:\n", ce)
+
         except requests.RequestException as re:
             print(f"Request error (attempt {attempt + 1}/{MAX_RETRIES}): {re}")
 
-            if attempt < MAX_RETRIES -1:
-                time.sleep(time_inactive_before_retry)
+            if attempt < MAX_RETRIES - 1:
                 print("Retrying....")
+                time.sleep(time_inactive_before_retry)
             else:
-                print( print("Max retries reached,unable to connect:\n",re))
+                print("Max retries reached,unable to connect:\n", re)
         except Exception as e:
-                print(e)
-                break
+            print(e)
+            break
+
 
 
 token = generate_token()
