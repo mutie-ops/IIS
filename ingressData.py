50e18849fc200707a5fc4ab1210fb65837aff502
Benjamin Mutie <benjaminmutie53@gmail.com>
2023-10-16 12:34:28 +0300

    moving to new machine

--- ingressData.py
+++ ingressData.py
@@ -12,7 +12,6 @@ def header_extraction(data):
 
 data_frame = header_extraction('C:\\Users\\Benjamin Mutie\\Desktop\\IIS\data\\test2.xlsx')
 
-
 def name_combination():
     first_name = data_frame['First Name']
     second_name = data_frame['Last Name']
@@ -24,68 +23,58 @@ name_combination()
 
 
 def name_hours_extraction():
-    unique_individuals = []
+    unique_individuals = data_frame['FULL NAMES'].unique()
     individual_hours = []
-    current_individual = None
-    total_hours = 0
-    for index, row in data_frame.iterrows():
-        full_names = row['FULL NAMES']
-        hours_worked = row['Work done in hours']
-
-        if full_names != current_individual:
-            if total_hours > 0:
-                unique_individuals.append(current_individual)
-                individual_hours.append(total_hours)
-            current_individual = full_names
-            total_hours = 0
-
-        if hours_worked > 0:
-            total_hours += 1
-
-    if total_hours > 0:
-        unique_individuals.append(current_individual)
+    for full_name in unique_individuals:
+        total_hours = (data_frame[data_frame['FULL NAMES'] == full_name]['Work done in hours'] > 0).sum()
         individual_hours.append(total_hours)
 
-    # Create a new DataFrame with unique full names and total hours worked
-    result_df = pd.DataFrame({'Full Name': unique_individuals, 'Days Worked': individual_hours})
+    result_df = pd.DataFrame({'FULL NAMES': unique_individuals, 'Days Worked': individual_hours})
     return result_df
 
 
 def overtime_extraction():
-    unique_day_type = []
-    number_of_day_types = []
-    current_day_type = None
-    total_day_types = 0
+    unique_day_types = data_frame['Day Type'].unique()
+    results = pd.DataFrame(columns=list(unique_day_types))
 
-    day_type = 'Day Type'
+    individual_totals = {}  # Dictionary to store individual totals for each day type
+
     for index, row in data_frame.iterrows():
-        if row[day_type] != current_day_type:
-            if total_day_types > 0:
-                unique_day_type.append(current_day_type)
-                number_of_day_types.append(total_day_types)
-            total_day_types = 0
+        full_name = row['FULL NAMES']
+        day_type = row['Day Type']
+        overtime = row['Overtime']
 
-        if pd.to_numeric(row['Day Type'], errors='coerce') > 0:
-            total_day_types += 1
+        if full_name not in individual_totals:
+            individual_totals[full_name] = {day_type: overtime}
+        else:
+            if day_type not in individual_totals[full_name]:
+                individual_totals[full_name][day_type] = overtime
+            else:
+                individual_totals[full_name][day_type] += overtime
 
-    if total_day_types > 0:
-        unique_day_type.append(current_day_type)
-        number_of_day_types.append(total_day_types)
+    rows_to_append = []
 
-    results = pd.DataFrame({'Day type': unique_day_type, 'total day types': number_of_day_types})
-    return results
+    for day_type_totals in individual_totals.values():
+        row = [day_type_totals.get(dt, 0) for dt in unique_day_types]
+        rows_to_append.append(row)
+
+    # create new columns OT 1.5 AND OT 2.0
+    overtime_1_5=
+    result_df = pd.concat([results, pd.DataFrame(rows_to_append, columns=results.columns)], ignore_index=True)
+
+    return result_df
 
 
 def save(savefile):
-    results = name_hours_extraction()
+    results1 = name_hours_extraction()
     results2 = overtime_extraction()
 
-    combined = pd.concat([results, results2], axis=0)
+    # Concatenate the DataFrames along the columns (axis=1)
+    combined = pd.concat([results1, results2], axis=1)
+
     combined.to_excel(savefile, index=False)
 
 
-save(savefile='output_excel_file3.xlsx')
-print('data saved')
-
-# WHEN YOU READ THIS I KNOW YOU WILL BE CONFUSED BUT HERE IS THE THING
-#  ARRANGE THE DAY TYPES IN ITS OWN COLUMN SO YOU CAN GET THE BEST RESULTS
+savefile = 'output_excel_file3.xlsx'
+save(savefile)
+print('Data saved')
